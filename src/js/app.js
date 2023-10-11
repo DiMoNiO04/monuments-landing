2aed2690a7990f2b6405ed731e0610a64c1ce8b9
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-10-11 21:00:57 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,5 @@ import './libs/inputmask.min.js';
 import './scripts/swipers.js';
 import './scripts/header.js';
 import './scripts/buttons.js';
-import './scripts/map.js';
 import './scripts/mask.js';
+import './scripts/map.js';
