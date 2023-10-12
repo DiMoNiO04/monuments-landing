7dd64b89fe2761710eeadfb389eaccd5719f6cf3
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-10-12 19:49:57 +0300

    feat: add webp func

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
+import { isWebp } from './modules/gulp.js';
+
 import './libs/inputmask.min.js';
-import './libs/gulp.js';
 
 import './scripts/swipers.js';
 import './scripts/header.js';
 import './scripts/buttons.js';
 import './scripts/mask.js';
 import './scripts/map.js';
+
+isWebp();
